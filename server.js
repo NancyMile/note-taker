29b81d6fcccf2cbb4fb1f29d72e80291de157249
nancy <smithnancy@outlook.com>
2022-08-21 17:37:25 +1000

    api/notes added on routes

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const path = require('path');
-const api = require('./public/assets/js/index.js');
+//const api = require('./public/assets/js/index.js');
 
 const notesData = require('./db/db.json');
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 //create an express application by calling
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+//app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -20,9 +20,9 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// app.get('api/notes', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/notes.html'))
-// );
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
