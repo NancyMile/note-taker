28c8e8b183ce8d677d149321a73c13516976a1f7
nancy <smithnancy@outlook.com>
2022-08-21 23:16:07 +1000

    server routes modified, post added

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-//const api = require('./public/assets/js/index.js');
 
 const notesData = require('./db/db.json');
 
@@ -11,18 +10,47 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use('/api', api);
 
 app.use(express.static('public'));
 
 //GET Route for homepage
-app.get('/', (req, res) =>
+app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/api/notes', (req, res) =>
+app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+// GET Route for retrieving all the notes
+app.get('/api/notes', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a new note`);
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newReview = {
+        title,
+        text,
+        //note_id: uuid(),
+      };
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting note');
+    }
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
